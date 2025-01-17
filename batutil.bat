f3ff9a8ea13e7a6e49ab4aeb44b710dd4b5281fc
SSgamingx123 <mushfiratnoor@gmail.com>
2025-01-17 23:08:08 +0600

    big changes

--- batutil.bat
+++ batutil.bat
@@ -1,33 +1,64 @@
 @echo off
+color 0a
 net session >nul 2>&1
-echo Administrative Privileges AQUIRED.
 if %errorLevel% NEQ 0 (
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /B
-)
-echo Downloading Windows Updates...
-:: powershell -Command "Install-WindowsUpdate -AcceptAll -AutoReboot"
-::remove this before deploying
-echo Creating apps.bat
-ping -n 3 youtube.com >nul
-::silent
-echo @echo off > apps.bat
-echo winget install discord --silent >> apps.bat
-echo winget install spotify --silent >> apps.bat 
-echo winget install whatsapp --silent >> apps.bat
-::idk what else to add yet
-
-:: Need to start winget application from non-administrator, don't know how. Tried this roundabout, didn't work.
-echo @echo off > start.bat
-echo start apps.bat >> start.bat
-start start.bat 
-
-pause
-code dump:
-:: If running with admin privileges, run your application
-if %errorLevel% NEQ 1 (
-    echo Running with administrative privileges...
-    start "" "C:\Program FIles\CrystalDiskInfo\DiskInfo64S.exe"
-)
-pause
-::might wanna try to implement auto detection.
+    Powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /B)
+echo =====================================
+echo || Launching Windows Update Script ||
+echo =====================================
+    echo @echo off > up.bat
+    echo Powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned >> up.bat
+    echo Powershell install-Module PSWindowsUpdate -Force >> up.bat
+    echo Powershell install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force >> up.bat
+    echo Powershell Import-Module PSWindowsUpdate >> up.bat
+    echo Powershell "Install-WindowsUpdate -AcceptAll -IgnoreRebootRequired" >> up.bat
+    echo pause >> up.bat
+start up.bat
+echo =====================
+echo || Downloading Apps||
+echo =====================
+    echo @echo off > ap.bat
+    echo winget install Discord.Discord --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install Spotify.Spotify --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install 9NKSQGP7F2NH --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install 9NBDXK71NK08 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install Valve.Steam --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install SoftDeluxe.FreeDownloadManager --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install CrystalDewWorld.CrystalDiskInfo --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install Microsoft.DotNet.DesktopRuntime.Preview --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install Oracle.JDK.21 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo winget install Discord.Discord --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> ap.bat
+    echo exit >> ap.bat
+    start ap.bat
+echo ==========================
+echo || Downloading Visual C ||
+echo ==========================
+    echo @echo off > vc.bat
+    echo winget install Microsoft.VCRedist.2005.x64 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2005.x86 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2008.x64 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2008.x86 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2010.x64 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2010.x86 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2012.x64 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2012.x86 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2013.x64 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2013.x86 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2015+.x64 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo winget install Microsoft.VCRedist.2015+.x86 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> vc.bat
+    echo exit >> vc.bat
+    start vc.bat
+echo =======================
+echo || Launching Websites||
+echo =======================
+    ::start "" "https://accounts.google.com/login"
+    ::start "" ""
+    ::start "" ""
+echo ================================
+echo || Restarting PC...           ||
+echo || Save all work and continue ||
+echo ================================
+pause >nul
+shutdown /r /d [p]2:3
--- batutillocal.bat
+++ batutillocal.bat
@@ -1,4 +1,5 @@
-@echo off
+@echo on
+color 0a
 net session >nul 2>&1
 if %errorLevel% NEQ 0 (
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
@@ -6,30 +7,30 @@ if %errorLevel% NEQ 0 (
 echo Downloading Windows Updates... this may take a while
     echo @echo off > up.bat
     echo powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned >> up.bat
-    echo powershell Install-Module PSWindowsUpdate -Force >> up.bat
-    echo powershell Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force >> up.bat
+    echo powershell install-Module PSWindowsUpdate -Force >> up.bat
+    echo powershell install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force >> up.bat
     echo powershell Import-Module PSWindowsUpdate >> up.bat
-    echo powershell -Command "Install-WindowsUpdate -AcceptAll -IgnoreRebootRequired" >> up.bat
-    echo exit >> up.bat
+    echo powershell -Command "Get-WindowsUpdate -AcceptAll -IgnoreRebootRequired" >> up.bat
+    echo pause >> up.bat
     start up.bat
 echo Windows Updates Finished
 
 echo Creating seperate bat for applications
 ping -n 3 youtube.com >nul
     echo @echo off > apps.bat
-    echo winget install Discord.Discord --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-    echo winget install Spotify.Spotify --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-    echo winget install 9NKSQGP7F2NH --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat 
-    echo winget install 9NBDXK71NK08 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-    echo winget install Valve.Steam --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-    echo winget install EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-    echo exit >> apps.bat
+    echo winget show Discord.Discord --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget show Spotify.Spotify --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget show 9NKSQGP7F2NH --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat 
+    echo winget show 9NBDXK71NK08 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget show Valve.Steam --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget show EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo pause >> apps.bat
 echo launching websites for login
     timeout 5
-    start "" "https://accounts.google.com/login"
+    ::start "" "https://accounts.google.com/login"
 start apps.bat
 
 :: Need to start winget application from non-administrator for some apps.. but HOW and WHY, don't know how. Tried this roundabout, didn't work.
 echo PC will restart, please save your work.
 pause
-shutdown /r /t 10 /d [p]2:3
+::shutdown /r /t 10 /d [p]2:3
