15ffb8c1a87bef690ba8dcb3be646e5cd179ba6c
SSgamingx123 <mushfiratnoor@gmail.com>
2025-01-17 15:10:02 +0600

    Windows update finished and consolidation

--- batutillocal.bat
+++ batutillocal.bat
@@ -5,29 +5,29 @@ if %errorLevel% NEQ 0 (
     exit /B)
 echo Downloading Windows Updates... this may take a while
     echo @echo off > up.bat
-    echo powershell Install-Module PSWindowsUpdate >> up.bat
-    echo powershell Import-Module PSWindowsUpdate -y >> up.bat
-    echo powershell -Command "Install-WindowsUpdate -AcceptAll" >> up.bat
+    echo powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned >> up.bat
+    echo powershell Install-Module PSWindowsUpdate -Force >> up.bat
+    echo powershell Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force >> up.bat
+    echo powershell Import-Module PSWindowsUpdate >> up.bat
+    echo powershell -Command "Install-WindowsUpdate -AcceptAll -IgnoreRebootRequired" >> up.bat
     echo exit >> up.bat
-    start up.bat    
+    start up.bat
+echo Windows Updates Finished
+
 echo Creating seperate bat for applications
-    ping -n 3 youtube.com >nul
-        echo @echo off > apps.bat
-        echo winget install Discord.Discord --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-        echo winget install Spotify.Spotify --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-        echo winget install 9NKSQGP7F2NH --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat 
-        echo winget install 9NBDXK71NK08 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-        echo winget install Valve.Steam --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-        echo winget install EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
-        :: ID's are whatsapp
-    start apps.bat
-echo Launching websites..
-timeout 30
+ping -n 3 youtube.com >nul
+    echo @echo off > apps.bat
+    echo winget install Discord.Discord --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget install Spotify.Spotify --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget install 9NKSQGP7F2NH --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat 
+    echo winget install 9NBDXK71NK08 --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget install Valve.Steam --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo winget install EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements --disable-interactivity --verbose >> apps.bat
+    echo exit >> apps.bat
+echo launching websites for login
+    timeout 5
     start "" "https://accounts.google.com/login"
-    start "" "microsoft"
-    start "" "whatsapp"
-    start "" "discord"
-::Winget needs to be non-admin for some apps for some reason.. idk how to do that yet
-echo Please login to all websites & save all work. PC will reboot.
+start apps.bat
+
+:: Need to start winget application from non-administrator for some apps.. but HOW and WHY, don't know how. Tried this roundabout, didn't work.
 pause
-shutdown /r /t 10 /d [p]2:3
